7aced800415a5b71953cadbe514c18aaf415e3bf
Aaron Sutton <hey@aaron.as>
2021-12-29 16:35:42 -0500

    Print style and toggle section

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,32 @@
-import './style.css'
+import "./style.css";
+
+// Assign event listeners to all expandable elements on the page.
+
+const sections = Array.from(
+  document.querySelectorAll("[data-expandable]")
+) as any;
+
+const controls = Array.from(document.querySelectorAll("[data-expand]"))
+
+controls.forEach((control) => {
+  var section: HTMLElement | undefined;
+
+
+  if (control instanceof HTMLElement) {
+
+    section = sections.find((e: any) => {
+      if (e instanceof HTMLElement) {
+        return control.dataset.expand === e.dataset.expandable;
+      } else {
+        return false;
+      }
+    });
+
+    control.addEventListener("click", () => {
+      if (section) {
+        section.toggleAttribute("data-open");
+      }
+    });
+
+  }
+});
